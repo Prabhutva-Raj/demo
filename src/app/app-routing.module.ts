a0a835afd50b595b9a0b4b07240ff30dd38f6f35
Prabhutva-Raj <40884972+Prabhutva-Raj@users.noreply.github.com>
2023-08-19 17:40:14 +0530

    Add files via upload

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
+
+const routes: Routes = [
+  {
+    path:'homepage',
+    component: HomepageComponent
+  },
+  {
+    path:'',
+    component: HomepageComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/homepage/homepage.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import content from './content.json';
+
+@Component({
+  selector: 'app-homepage',
+  templateUrl: './homepage.component.html',
+  styleUrls: ['./homepage.component.css']
+})
+export class HomepageComponent {
+  para1:any = content.para1;
+  para2:any = content.para2;
+  para3:any = content.para3;
+  para4:any = content.para4;
+  para5:any = content.para5;
+  benefits:any = content.benefits;
+
+  telegram(){
+    
+  }
+
+
+}
